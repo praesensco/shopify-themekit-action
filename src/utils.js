1e6d2751dd8e1a3d7970574e6caaf5a736445d59
Michał Jóźwin <michal@praesens.co>
2020-06-24 14:43:15 +0200

    Truncate theme name to 50 chars as there is a limitation in the Shopify theme name length

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 const normalizeThemeName = function normalizeShopifyThemeName(name) {
-   // Remove "refs/heads/" from start of string
-  return name.replace(/^refs\/heads\//g, '')
+   // - remove "refs/heads/" from start of string
+   // - truncate to 50 chars as there is a limitation in the Shopify theme name length
+  return name.replace(/^refs\/heads\//g, '').substr(0, 50);
 };
 
 module.exports = {
